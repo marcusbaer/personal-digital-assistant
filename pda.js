96e7dafdc6ab4d4b8873591aa2af7ad4f0abde47
Marcus Baer <marcus.baer@publicispixelpark.de>
2021-12-10 01:05:39 +0100

    config file

--- pda.js
+++ pda.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const options = require('./pda.json')
+console.log(process.cwd(), options)
+
 const [node, pda, ...params] = process.argv
 const message = params.join(' ')
 console.log('Hello from PDA', message)
